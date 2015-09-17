5d776593c5754437cf5856b7e852f1d5d6f6826f
apatheiagit <apatheiar@mail.ru>
2015-09-17 09:58:49 +0300

    Хлебные крошки и кнопка Наверх

--- js/main.js
+++ js/main.js
@@ -23,7 +23,19 @@ $(function(){
     }, function(){
       $(this).parent().children('a').removeClass('hover');;
     });
-
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 0) {
+            $('.icon-to-top').fadeIn();
+        } else {
+            $('.icon-to-top').fadeOut();
+        }
+    });
+    $('.icon-to-top').click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 400);
+        return false;
+    });
 })
 function getWeather(city){		
 	$.ajax({
